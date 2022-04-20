55ca59f525b39abf24a0723a584fd704d05ca256
joao <joao.otavio00@hotmail.com>
2022-04-19 21:42:21 -0300

    primeiro exercicio mod3

---
+++ modulo3/projeto-whatslab/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id='principal' className="container">
+
+      <div><input className='usuario' placeholder='Usuário'></input></div>
+
+      <div><input className='mensagem' placeholder='Mensagem'></input></div>
+
+      <div><button className='botao'>Enviar</button></div>
+    </div>
+  )
+}
+
+export default App
