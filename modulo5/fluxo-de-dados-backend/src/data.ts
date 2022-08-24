c583ef787ba30955e93342e3f31639d9e112af58
joao <joao.otavio00@hotmail.com>
2022-08-23 21:10:34 -0300

    Fluxo de dados

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,17 @@
+export const products: any = [
+    {
+        id: '1',
+        name: 'banana',
+        price: 2
+    },
+    {
+        id: '2',
+        name: 'leite',
+        price: 5
+    },
+    {
+        id: '3',
+        name: 'acucar',
+        price: 4
+    }
+]
